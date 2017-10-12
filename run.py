92b02fae092a6b24d004cf8a07ba55e4353675cf
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2017-10-12 22:06:24 +0200

    added zip download function

--- run.py
+++ run.py
@@ -1,23 +1,48 @@
 import os
-from flask import Flask, request, redirect, url_for
-from werkzeug import secure_filename
+from os.path import basename
+from flask import Flask, request, redirect, url_for, flash, send_file
+from werkzeug.utils import secure_filename
+import io
+import zipfile
 
-UPLOAD_FOLDER = '/tmp/'
+UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = set(['jpg', 'png'])
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # maximum file size 16MB
+
+# create upload folder if it does not exist
+os.makedirs(os.path.join(app.config['UPLOAD_FOLDER']), exist_ok=True)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+@app.route("/download")
+def download_files():
+    memory_file = io.BytesIO()
+    with zipfile.ZipFile(memory_file, 'w') as zf:
+        files = os.listdir(os.path.join(app.root_path, "uploads"))
+        for individualFile in files:
+            path = os.path.join(app.root_path, "uploads", individualFile)
+            zf.write(path, basename(path)) # only add last part of our path to zip --> image file!
+    memory_file.seek(0)
+    return send_file(memory_file, attachment_filename='files.zip', as_attachment=True)
+
 @app.route("/", methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST' and "file[]" in request.files:
-        for f in request.files.getlist("file[]"):
-            print(f.filename, allowed_file(f.filename))
-            print(request.form["building_id"])
-            print(request.form["text_name"])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if not request.files.getlist("file[]"):
+            flash('No file part')
+            return redirect(request.url)
+        files = request.files.getlist('file[]')
+        for f in files:
+            if f and allowed_file(f.filename):
+                print(f)
+                filename = secure_filename(f.filename)
+                f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return """
     <!DOCTYPE html>
 <html lang="en">
@@ -71,6 +96,7 @@ def index():
               </label> 
             </li>
             <li><button>hochladen</button></li>
+            <li><a href="/download">Click me.</a></li>
         
         </ul>
         
