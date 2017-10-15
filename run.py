66147e310f5fb50fbaf27218783c29fbe041c42e
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2017-10-15 12:27:01 +0200

    added cleanup mechanism

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from os.path import basename
 from flask import Flask, request, redirect, url_for, flash, send_file
 from werkzeug.utils import secure_filename
 import io
+import shutil
 from zipfile import ZipFile
 
 from convert import addIcon
@@ -15,8 +16,9 @@ app.secret_key = os.urandom(24)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # maximum file size 16MB
 
-# create upload folder if it does not exist
+# SETUP FOLDERS
 os.makedirs(os.path.join(app.config['UPLOAD_FOLDER']), exist_ok=True)
+os.makedirs(os.path.join("converted"), exist_ok=True)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -25,12 +27,21 @@ def allowed_file(filename):
 def download_files():
     memory_file = io.BytesIO()
     with ZipFile(memory_file, 'w') as zf:
-        files = os.listdir(os.path.join(app.root_path, "uploads"))
+        files = os.listdir(os.path.join(app.root_path, "converted"))
         for individualFile in files:
-            path = os.path.join(app.root_path, "uploads", individualFile)
-            zf.write(path, basename(path)) # only add last part of our path to zip --> image file!
+            folderpath = os.path.join(app.root_path, "converted", individualFile)
+            for f in os.listdir(folderpath):
+                filepath = os.path.join(folderpath, f)
+                zf.write(filepath, basename(filepath)) # only add last part of our path to zip --> image file!    
     memory_file.seek(0)
-    return send_file(memory_file, attachment_filename='files.zip', as_attachment=True)
+
+    # clean up & recreate directories
+    shutil.rmtree(os.path.join("converted"))
+    shutil.rmtree(app.config["UPLOAD_FOLDER"])
+    os.makedirs(os.path.join(app.config['UPLOAD_FOLDER']), exist_ok=True)
+    os.makedirs(os.path.join("converted"), exist_ok=True)
+
+    return send_file(memory_file, attachment_filename='{}.zip'.format(basename(folderpath)), as_attachment=True)
 
 @app.route("/", methods=['GET', 'POST'])
 def upload_file():
@@ -39,19 +50,22 @@ def upload_file():
         if not request.files.getlist("file[]"):
             flash('No file part')
             return redirect(request.url)
+
         files = request.files.getlist('file[]')
+        building = request.form["building_id"]
+        company = request.form["text_name"]
+        
         for f in files:
             if f and allowed_file(f.filename):
                 print(f)
                 filename = secure_filename(f.filename)
                 f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        # convert uploaded files now
         files = os.listdir(os.path.join(app.root_path, "uploads"))
         for individualFile in files:
             path = os.path.join(app.root_path, "uploads", individualFile)
-            building = request.form["building_id"]
-            company = request.form["text_name"]
             img, directory, outfile = addIcon(path, building, company, os.path.join(app.root_path, "converted"))
-            print(img, os.path.join(directory,outfile))
             img.save(os.path.join(directory,outfile), "JPEG")
 
     return """
