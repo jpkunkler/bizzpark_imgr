5081ad9f581053c2efe89253e4b29e03314f8f25
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2018-04-05 17:22:00 +0200

    added size selection

--- create_icons.py
+++ create_icons.py
@@ -6,10 +6,21 @@ directory = "icons/"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-def createIcon(num):
-	fnt = ImageFont.truetype("OpenSans-Bold.ttf", size=150)
+def createIcon(num, size):
 
-	W, H = (200,200)
+	# check for size (properties: sm = small, lg = large, xl = extra-large)
+	if size == "sm":
+		fnt = ImageFont.truetype("OpenSans-Bold.ttf", size=150)
+		W, H = (200,200)
+		offset = 20
+	elif size == "lg":
+		fnt = ImageFont.truetype("OpenSans-Bold.ttf", size=375)
+		W, H = (500,500)
+		offset = 50
+	elif size == "xl":
+		fnt = ImageFont.truetype("OpenSans-Bold.ttf", size=750)
+		W, H = (1000,1000)
+		offset = 100
 
 	# our number format is 01-09 and then 10-25
 	# so we need to add a zero-digit to our integers < 10!
@@ -20,11 +31,11 @@ def createIcon(num):
 
 	im = Image.new("RGB",(W,H), (255,0,0))
 	draw = ImageDraw.Draw(im)
-	draw.rectangle(((0, 199), (199, 0)), outline=(255,255,255))
+	draw.rectangle(((0, W-1), (H-1, 0)), outline=(255,255,255))
 	w, h = fnt.getsize(msg)
-	draw.text(((W-w)/2,(H-h)/2 - 20), msg, font=fnt,fill=(255, 255, 255))
+	draw.text(((W-w)/2,(H-h)/2 - offset), msg, font=fnt,fill=(255, 255, 255))
 
 	im.save("icons/{}.png".format(msg), "PNG")
 
 for i in range(1, 25):
-	createIcon(i)
+	createIcon(i, "lg")
